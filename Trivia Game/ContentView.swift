0a9f9fc03960611e16669d7a085c3db73457d8ed
Shayan Manoharan <shayanmanoharan@gmail.com>
2026-02-11 15:50:04 -0500

    Initial Commit

---
+++ Trivia Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trivia Game
+//
+//  Created by Shayan Manoharan on 2/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trivia GameTests/Trivia_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_GameTests.swift
+//  Trivia GameTests
+//
+//  Created by Shayan Manoharan on 2/11/26.
+//
+
+import Testing
+@testable import Trivia_Game
+
+struct Trivia_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
